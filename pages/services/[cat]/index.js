85da6901308691bbac596b5e65b15b6c9fa2297e
jeanpierre <jean.gonzales1414@gmail.com>
2022-12-10 21:53:18 -0500

    Changes to index.js within cat folder

--- pages/services/[cat]/index.js
+++ pages/services/[cat]/index.js
@@ -1,32 +1,46 @@
-const ServicesCatPage = () => {
+import Image from "next/image";
+
+const ServicesCatPage = ({ data }) => {
   return (
     <div>
-      <h1>Course List</h1>
-      <a href="">
-        <img />
-        <h2>Event 1</h2>
-      </a>
-      <a href="">
-        <img />
-        <h2>Event 2</h2>
-      </a>
-      <a href="">
-        <img />
-        <h2>Event 3</h2>
-      </a>
-      <a href="">
-        <img />
-        <h2>Event 4</h2>
-      </a>
-      <a href="">
-        <img />
-        <h2>Event 5</h2>
-      </a>
-      <a href="">
-        <img />
-        <h2>Event 6</h2>
-      </a>
+      <h1>Courses Offered</h1>
+      <div>
+        {data.map((ev) => (
+          <a key={ev.id} href={`/services/${ev.service}/${ev.id}`}>
+            <Image width={300} height={200} alt={ev.image} />
+            <h2>{ev.title}</h2>
+            <p>{ev.description}</p>
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
 export default ServicesCatPage;
+
+export async function getStaticPaths() {
+  const { service_categories } = await import("/data/data.json");
+
+  const allPaths = service_categories.map((ev) => {
+    return {
+      params: {
+        cat: ev.id.toString(),
+      },
+    };
+  });
+  return {
+    paths: allPaths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const id = context?.params.cat;
+  const { allservices } = await import("/data/data.json");
+
+  const data = allservices.filter((ev) => ev.service === id);
+
+  return {
+    props: { data },
+  };
+}
