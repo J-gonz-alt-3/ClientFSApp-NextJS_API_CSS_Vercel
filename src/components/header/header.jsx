8bb929ae858e9481f6a859c5fe0830700ee7ded0
jeanpierre <jean.gonzales1414@gmail.com>
2022-12-12 12:42:46 -0500

    styling css file general.css

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,22 +1,45 @@
 import Link from "next/link";
+import Image from "next/image";
+
 export const Header = () => {
   return (
     <header>
-      <nav>
-        <img />
-        <Link href="/" passHref>
-          Home
-        </Link>
-        <Link href="/services" passHref>
-          Services
-        </Link>
-        <Link href="/about-us" passHref>
-          About Us
-        </Link>
-        <Link href="/contact-us" passHref>
-          Contact Us
-        </Link>
-      </nav>
+      <div>
+        <div className="topNav">
+          <Image
+            alt="logo"
+            src={"/../public/logo.jpg"}
+            width={75}
+            height={75}
+          />
+          <nav>
+            <ul>
+              <li>
+                {" "}
+                <Link href="/" passHref>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link href="/services" passHref>
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link href="/about-us" passHref>
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link href="/contact-us" passHref>
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <h1>IT Global Professionals</h1>
+      </div>
     </header>
   );
 };
--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export const HomePage = ({ data }) => {
-  return (
-    <main>
-      {data.map((ev) => (
-        <Link key={ev.id} href={`/services/${ev.id}`} passHref>
-          <Image width={300} height={200} alt={ev.title} src={ev.image} />
+export const HomePage = ({ data }) => (
+  <div className="home_body">
+    {data.map((ev) => (
+      <Link className="card" key={ev.id} href={`/services/${ev.id}`} passHref>
+        <div className="image">
+          <Image width={400} height={300} alt={ev.title} src={ev.image} />
+        </div>
+        <div className="content">
           <h2>{ev.title}</h2>
           <p>{ev.description}</p>
-        </Link>
-      ))}
-    </main>
-  );
-};
+        </div>
+      </Link>
+    ))}
+  </div>
+);
