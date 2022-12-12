49d7fd351b3f91486399398cdd5695f19cec6728
jeanpierre <jean.gonzales1414@gmail.com>
2022-12-12 14:24:04 -0500

    styling css file general.css

--- src/components/services/catService.jsx
+++ src/components/services/catService.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-const CatService = () => {
+const CatService = ({ data, pageName }) => {
   return (
-    <div>
+    <div className="cat_services">
       <h1>Services for {pageName}</h1>
       <div>
         {data.map((ev) => (
           <Link key={ev.id} href={`/services/${ev.service}/${ev.id}`} passHref>
-            <Image width={300} height={300} alt={ev.title} src={ev.image} />
-            <h2>{ev.title}</h2>
-            <p>{ev.description}</p>
+            <p className="card">
+              <Image width={300} height={300} alt={ev.title} src={ev.image} />
+              <h2>{ev.title}</h2>
+              <p>{ev.description}</p>
+            </p>
           </Link>
         ))}
       </div>
