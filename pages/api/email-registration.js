d5f776e05b1fb3f96e32fbdf42fda6f59e95a77c
jeanpierre <jean.gonzales1414@gmail.com>
2022-12-12 20:17:54 -0500

    finishing up api for user registration for courses

---
+++ pages/api/email-registration.js
@@ -0,0 +1,63 @@
+import path from "path";
+import fs from "fs";
+
+function buildPath() {
+  return path.join(process.cwd(), "data", "data.json");
+}
+
+function extractData(filePath) {
+  const jsonData = fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
+}
+
+export default function handler(req, res) {
+  const { method } = req;
+  // Access our data and
+  // extract our Data (allservices).
+  // res 404 if there are no allservices.
+  // allservices - loop through them and identify the serviceID,
+  // and add the email into emails_registered,
+  // only if that email doesn't already exist.
+  // check the format of the email to make sure it is valid.
+
+  const filePath = buildPath();
+  const { service_categories, allservices } = extractData(filePath);
+
+  if (!allservices) {
+    return res.status(404).json({
+      status: 404,
+      message: "Services data not found",
+    });
+  }
+
+  if (method === "POST") {
+    const { email, eventId } = req.body;
+
+    const newAllServices = allservices.map((ev) => {
+      if (ev.id === eventId) {
+        if (ev.emails_registered.includes(email)) {
+          res
+            .status(201)
+            .json({ message: "This email has already been registered" });
+        }
+        return {
+          ...ev,
+          emails_registered: [...ev.emails_registered, email],
+        };
+      }
+      return ev;
+    });
+    fs.writeFileSync(
+      filePath,
+      JSON.stringify({
+        service_categories,
+        allservices: newAllServices,
+      })
+    );
+    // add our code here
+    res.status(200).json({
+      message: `You have been registered successfully with the following email address: ${email} ${eventId}`,
+    });
+  }
+}
--- src/components/services/single-service.jsx
+++ src/components/services/single-service.jsx
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export const SingleService = ({ data }) => {
+  const inputEmail = useRef();
+  const router = useRouter();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const emailValue = inputEmail.current.value;
+    const eventId = router?.query.id;
+
+    try {
+      const response = await fetch("/api/email-registration", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: emailValue, eventId }),
+      });
+      if (!response.ok) {
+        throw new Error(`ERROR: ${response.status}`);
+      }
+      const data = await response.json();
+      //POST fetch request
+      // body emailValue and the eventId
+    } catch (e) {
+      console.log("ERROR", e);
+    }
+  };
+
   return (
-    <div>
-      <Image src={data.image} width={800} height={500} alt={data.title} />
+    <div className="service_single_page">
       <h1>{data.title}</h1>
+      <Image src={data.image} width={800} height={500} alt={data.title} />
+
       <p>{data.description}</p>
-      <input type="email" />
-      <button>Submit</button>
+      <form onSubmit={onSubmit} className="email_registration">
+        <label>Reserve a Seat</label>
+        <input
+          ref={inputEmail}
+          type="email"
+          id="email"
+          placeholder="Type email here"
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
