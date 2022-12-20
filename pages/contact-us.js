56744169f2793eaacade5fdabb46a0a8c9ab57b9
jeanpierre <jean.gonzales1414@gmail.com>
2022-12-19 22:10:39 -0500

    adding responsiveness

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -2,24 +2,37 @@ import Image from "next/image";
 
 const ContactUsPage = () => {
   return (
-    <div>
-      <h1>Contact Us</h1>
-      <div>
-        <Image
-          src={"/contactus.jpg"}
-          alt={"contact photo"}
-          width={300}
-          height={400}
-        />
+    <section className="p-5">
+      <div className="container">
+        <div className="row g-4">
+          <div className="col-md">
+            <h2 className="text-cener mb-4">Contact Us</h2>
+            <ul className="list-group list-group-flush lead">
+              <li className="list-group-item">
+                <span className="fw-bold">Main Location:</span> 50 Main St.
+                Boston, MA
+              </li>
+              <li class="list-group-item">
+                <span class="fw-bold">Student Phone:</span> (333) 333-3333
+              </li>
+              <li class="list-group-item">
+                <span class="fw-bold">Enrollment Email:</span>
+                itglobalpro@itglobalpro.com
+              </li>
+            </ul>
+            <div className="col md">
+              <Image
+                src={"/contactus.jpg"}
+                alt={"contact photo"}
+                width={400}
+                height={500}
+                className="img-fluid rounded float-left"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <div>
-        <p>
-          123-456-7890
-          <br />
-          ITGlobalpro.com
-        </p>
-      </div>
-    </div>
+    </section>
   );
 };
 export default ContactUsPage;
